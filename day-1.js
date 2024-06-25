45e38319515aa86a47efb6610f97f3596df235f4
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-06-24 19:25:37 -0500

    Completed Day 1

--- day-1.js
+++ day-1.js
@@ -11,11 +11,11 @@ function day1Part1() {
   playerOne.hiScore = prompt("What is your all time hi-score?");
   // Try it!
   // TODO: add a new question for the player. Assign the answer to the playerOne Object.
-
+  playerOne.superpower = prompt("What is your superpower?");
 
   // Try it!
   // TODO: add the answer to the new question here.
-  messageParagraph.innerHTML = `Hi, ${playerOne.name}! Can you beat your hi score of ${playerOne.hiScore}?`;
+  messageParagraph.innerHTML = `Hi, ${playerOne.name}! Can you beat your hi score of ${playerOne.hiScore} with your superpower of ${playerOne.superpower}?`;
 
 }
 
@@ -28,12 +28,12 @@ function day1Part2() {
   customer.address = prompt("What is your address?");
   // Try it!
   // TODO: add a new question for the player. Assign the answer to the playerOne Object.
-  
+  customer.email = prompt("What is your email address?");
 
   
   // Try it!
   // TODO: add the answer to the new question to the innerHTML statment below.
-  messageParagraph.innerHTML = `Hi, ${customer.name}! Your current address is ${customer.address}.`;
+  messageParagraph.innerHTML = `Hi, ${customer.name}! Your current address is ${customer.address}. We will send you updates to this email ${customer.email}.`;
 }
 part2.onclick = day1Part2;
 
@@ -44,10 +44,12 @@ function day1Part3() {
 
   // Try it!
   // TODO: create a prompt to ask about a pet
+  pet.name = prompt("What is the name of your pet?");
+  pet.treat = prompt("What is your pet's favorite treat?");
 
   // Try it!
   // TODO: make a message based on the answers to the prompt.
-  messageParagraph.innerHTML = ``;
+  messageParagraph.innerHTML = `Your pet's name is ${pet.name}. And they love ${pet.treat}.`;
 }
 part3.onclick = day1Part3;
 
@@ -55,7 +57,18 @@ part3.onclick = day1Part3;
 // Try it!
 // TODO: add your own idea for a prompt. Use an object to store the user's response.
 function day1Part4() {
-  alert("hello!");
+  let pizzaOrder;
+  pizzaOrder = {};
+
+  pizzaOrder.customerName = prompt("What is the name for the order?");
+  pizzaOrder.pizzaSize = prompt("What size pizza would you like?");
+  pizzaOrder.toppings = prompt("What toppings would you like?");
+
+  messageParagraph.innerHTML = `Hi ${pizzaOrder.customerName}! Your ${pizzaOrder.pizzaSize} pizza with ${pizzaOrder.toppings} toppings, will be ready for pick-up in 35 minutes.`
+
+
+
+
 
   
 }
